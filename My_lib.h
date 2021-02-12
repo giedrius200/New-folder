e3269751aacc2631fdd43e45cb65067dc8a078fe
Giedrius <linas.kaliosas@gmail.com>
2021-02-12 11:16:29 +0200

    labas

---
+++ My_lib.h
@@ -0,0 +1,18 @@
+#ifndef MY_LIB_H_INCLUDED
+#define MY_LIB_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <stdio.h>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+void mano_isv(string);
+
+#endif 
